a6c11b061b5e43ca96a6d4f4218a3bdf9252f1c3
Michael Chu <mcchu95@gmail.com>
2020-06-12 03:10:16 -0400

    Add functions to get weather

--- app/src/App.js
+++ app/src/App.js
@@ -3,33 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-const apiKey = 'zflbL8ybrrRPylLVUi2YI0gwcGbprkcu'
+const apiKey = 'zflbL8ybrrRPylLVUi2YI0gwcGbprkcu';
 
 class App extends React.Component {
+  state = { value: '', weather: '' };
+
+  getLocationKey = (query) => {
+    return axios.get(
+      `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${query}`
+    );
+  };
+
   getWeather(input) {
-    axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${input}?apikey=${apiKeys}`)
-      .then(res => {
-        console.log(res);
-      })
-  }
-
-  componentDidMount() {
-    document.addEventListener('keydown', (event) => {
-      if(event.keyCode === 13){
-        this.getWeather()
-      }
-    });
-  }
-
-  render() {
-    return (
-      <form>
-        <label for="city">City Name</label>
-        <input type="text" name="city"></input>
-      </form>
+    return axios.get(
+      `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${input}?apikey=${apiKey}`
     );
   }
 
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const locationKey = (await this.getLocationKey(this.state.value)).data[0]
+      .Key;
+    const weather = await this.getWeather(locationKey);
+
+    console.log(weather);
+
+    this.setState({ weather: weather.data.DailyForecasts[0].Day.IconPhrase });
+
+    console.log(this.state.weather);
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="city">City Name</label>
+          <input
+            type="text"
+            name="city"
+            value={this.state.value}
+            onChange={this.handleChange}
+          ></input>
+        </form>
+
+        <div>{this.state.weather}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
